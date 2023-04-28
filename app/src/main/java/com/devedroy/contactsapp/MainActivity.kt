2694b8df9e38dcbff8a71ce977146d3b7f0fae2e
devedroy <devedroy.dr@gmail.com>
2023-04-28 21:59:31 +0530

    Added Fragment Navigation

--- app/src/main/java/com/devedroy/contactsapp/MainActivity.kt
+++ app/src/main/java/com/devedroy/contactsapp/MainActivity.kt
@@ -2,10 +2,35 @@ package com.devedroy.contactsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import com.devedroy.contactsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        navController = findNavController(R.id.navHostFragment)
+        navController.navigate(R.id.contactsFragment)
+
+        mainBinding.bottomNavView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_item_contact -> {
+                    navController.navigate(R.id.action_contact)
+                    true
+                }
+
+                R.id.menu_item_message -> {
+                    navController.navigate(R.id.action_message)
+                    true
+                }
+
+                else -> false
+            }
+        }
     }
 }
